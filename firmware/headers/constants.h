b463e93579ac2564a457d8db4dca45b51426e789
jamiekang7 <jamiekang0@gmail.com>
2026-02-07 10:50:27 -0800

    updated pin configuration

--- firmware/headers/constants.h
+++ firmware/headers/constants.h
@@ -1,7 +1,7 @@
-#define PIN_UH 25
-#define PIN_UL 33
-#define PIN_VH 27
-#define PIN_VL 26
-#define PIN_WH 12
-#define PIN_WL 14
-#define PIN_EN 32
+#define PIN_UL 4
+#define PIN_UH 5
+#define PIN_VL 6
+#define PIN_VH 7
+#define PIN_WL 15
+#define PIN_WH 16
+#define PIN_EN 17
