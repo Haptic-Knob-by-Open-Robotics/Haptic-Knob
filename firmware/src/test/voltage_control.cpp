da484ccdd844be2151b9ec43de83385f120435fa
jamiekang7 <jamiekang0@gmail.com>
2026-03-05 19:09:26 -0800

    updated voltage torque control

--- firmware/src/test/voltage_control.cpp
+++ firmware/src/test/voltage_control.cpp
@@ -1,102 +1,114 @@
 #include <Arduino.h>
 #include <SimpleFOC.h>
-#include "../headers/MagneticSensorMT6701SSI.h"
-#include "../headers/constants.h"
+#include "app/Config.h"
+#include "drivers/ModifiedMagneticSensorMT6701SSI.h"
+#include "drivers/SpiBus.h"
 
-BLDCMotor motor = BLDCMotor(7);  // 7 pole pairs 
+SpiBus spiBus(PIN_SPI_CLK, PIN_SPI_MISO, PIN_SPI_MOSI);
+ModifiedMagneticSensorMT6701SSI encoder(PIN_ENC_CS);
+InlineCurrentSense current_sense(SHUNT_RESISTOR, AMP_GAIN, PIN_I_A, PIN_I_B, PIN_I_C);
+BLDCDriver6PWM driver(PIN_UH, PIN_UL, PIN_VH, PIN_VL, PIN_WH, PIN_WL);
+BLDCMotor motor(4);
 
-BLDCDriver6PWM driver = BLDCDriver6PWM(
-    PIN_VH, PIN_VL,
-    PIN_UH, PIN_UL,
-    PIN_WH, PIN_WL,
-    PIN_EN
-);
+float target_voltage_torque = 0.2f;
 
-MagneticSensorMT6701SSI encoder(PIN_ENC_CS);
-Commander command = Commander(Serial);
+void printData()
+{
+//   encoder.update();
 
-void doTarget(char* cmd) { 
-    command.scalar(&motor.target, cmd);
-    Serial.print("Target: ");
-    Serial.print(motor.target, 2);
-    Serial.println(" V");
+//   Serial.printf(
+//       "Angle: %7.2f deg | Vel: %7.3f rad/s | ShaftAngle: %7.3f rad\n",
+//       encoder.angleDegWrapped(),
+//       encoder.getVelocity(),
+//       motor.shaft_angle
+//   );
+Serial.printf(
+      "Angle: %7.2f deg | Vel: %7.3f rad/s | ShaftAngle: %7.3f rad\n",
+      motor.shaft_angle * 180.0f / _PI,
+      motor.shaft_velocity,
+      motor.shaft_angle
+  );
 }
 
-void setup() {
-    Serial.begin(115200);
-    delay(3000);
-    
-    Serial.println("\nFOC VOLTAGE CONTROL\n");
-    
-    encoder.init();
-    motor.linkSensor(&encoder);
-    
-    driver.pwm_frequency = 30000;
-    driver.dead_zone = 0.05;
-    driver.voltage_power_supply = 12;
-    driver.voltage_limit = 12;
-    driver.init();
-    motor.linkDriver(&driver);
-    
-    motor.useMonitoring(Serial);
-    motor.init();
-    
-    // Auto calibrate zero angle
-    Serial.println("Calibrating zero angle...");
-    motor.controller = MotionControlType::velocity_openloop;
-    motor.voltage_limit = 2.0;
-    motor.target = 2.0;
-    
-    for(int i = 0; i < 150; i++) {
-        motor.move();
-        delay(10);
-    }
-    
-    float shaft_angle = motor.shaft_angle;
-    motor.target = 0;
-    for(int i = 0; i < 50; i++) {
-        motor.move();
-        delay(10);
-    }
-    
-    float zero_elec = _normalizeAngle(_electricalAngle(shaft_angle, motor.pole_pairs));
-    
-    // Configure FOC voltage mode
-    motor.controller = MotionControlType::torque;
-    motor.torque_controller = TorqueControlType::voltage;
-    motor.voltage_limit = 3.0;
-    
-    motor.zero_electric_angle = zero_elec;
-    motor.sensor_direction = Direction::CCW;  // CCW 
-    
-    Serial.print("Zero angle: ");
-    Serial.println(zero_elec, 2);
-    Serial.println("Sensor direction: CCW");
-    Serial.println("Pole pairs: 7");
-    
-    command.add('T', doTarget, "voltage");
-    
-    Serial.println("\n=== MOTOR READY ===");
-    Serial.println("Commands:");
-    Serial.println("  T0.5  - Apply 0.5V");
-    Serial.println("  T1    - Apply 1V");
-    Serial.println("  T2    - Apply 2V");
-    Serial.println("  T0    - Release");
+void setup()
+{
+  Serial.begin(115200);
+  delay(3000);
+  SimpleFOCDebug::enable(&Serial);
+  Serial.println("=== FOC Voltage Torque Test ===");
+
+  spiBus.init();
+  encoder.init(spiBus.bus());
+  Serial.println("Encoder initialized");
+
+  driver.voltage_power_supply = SUPPLY_VOLTAGE;
+  driver.voltage_limit = VOLTAGE_LIMIT;
+  driver.pwm_frequency = 30000;
+  driver.dead_zone = 0.05f;
+
+  Serial.print("Driver init... ");
+  if (!driver.init())
+  {
+    Serial.println("FAILED");
+    while (1) {}
+  }
+  Serial.println("done");
+
+  motor.linkDriver(&driver);
+  motor.linkSensor(&encoder);
+
+  motor.controller = MotionControlType::torque;
+  motor.torque_controller = TorqueControlType::voltage;
+
+  motor.voltage_limit = VOLTAGE_LIMIT;
+  motor.velocity_limit = 20.0f;
+
+  motor.voltage_sensor_align = 3.0f;
+  motor.voltage_limit = 3.0f;
+  motor.velocity_limit = 20.0f;
+
+  Serial.print("Motor init... ");
+  motor.init();
+  Serial.println("done");
+
+  Serial.print("FOC init... ");
+  if (!motor.initFOC())
+  {
+    Serial.println("FAILED");
+    while (1) {}
+  }
+  Serial.println("done");
+
+  Serial.println("Starting test");
 }
 
-void loop() {
-    motor.loopFOC();
-    motor.move();
-    command.run();
-    
-    static unsigned long last = 0;
-    if(millis() - last > 1000) {
-        Serial.print("Target: ");
-        Serial.print(motor.target, 2);
-        Serial.print(" V | Angle: ");
-        Serial.print(encoder.getAngle(), 2);
-        Serial.print(" | Velocity: ");
-        Serial.println(encoder.getVelocity(), 2);
-        last = millis();
-    }
+void loop()
+{
+//   motor.loopFOC();
+//   motor.move(target_voltage_torque);
+
+//   static uint32_t last_print = 0;
+//   static uint32_t last_flip = 0;
+//   uint32_t now = millis();
+
+//   if (now - last_flip > 3000)
+//   {
+//     last_flip = now;
+//     target_voltage_torque = -target_voltage_torque;
+//   }
+
+//   if (now - last_print > 100)
+//   {
+//     last_print = now;
+//     printData();
+//   }
+  motor.loopFOC();
+  motor.move(target_voltage_torque);
+
+  static uint32_t last_print = 0;
+  if (millis() - last_print > 100)
+  {
+    last_print = millis();
+    printData();
+  }
 }
